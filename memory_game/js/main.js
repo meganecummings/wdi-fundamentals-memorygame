7a5f166d039095801c0e0b97db8b3333ce4979be
Meg Cummings <megcummings@gmail.com>
2019-06-13 17:01:12 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,18 +1,13 @@
+/* 
 console.log("Up and running!");
 
-/* var cardOne = "queen"
+var cardOne = "queen"
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardThree);
-
-*/
-
-var cards = ['queen', 'queen', 'king', 'king'];
-
-var cardsInPlay = [];
 
 var cardOne = cards[0];
 cardsInPlay.push(cardOne);
@@ -21,14 +16,34 @@ console.log("User flipped " + cardOne);
 var cardTwo = cards[1];
 cardsInPlay.push(cardTwo);
 
-console.log("User flipped " + cardTwo);
+*/
 
-console.log(cardsInPlay);
+var cards = ['queen', 'queen', 'king', 'king'];
 
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
- alert("You found a match!");
-} else {
-	alert("Sorry, try again.");
+var cardsInPlay = [];
+
+function checkForMatch() {
+	if (cardsInPlay.length ==2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You found a match!");
+	} else {
+		console.log("Sorry, try again!");
+	}
 }
+};
+
+function flipCard(cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+}
+
+
+flipCard(0);
+flipCard(2);
+
+
+
+
 
 
