46bc3e38a703b4cf301e3d5724370a3796e52d1d
Meg Cummings <megcummings@gmail.com>
2019-06-16 17:21:16 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -46,27 +46,36 @@ var cards = [
 var cardsInPlay = [];
 
 function checkForMatch() {
-	if (cardsInPlay.length ==2) {
+	if (cardsInPlay.length == 2) {
 		if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	} else {
-		alert("Sorry, try again!");
+			alert("You found a match!");
+		} else {
+			alert("Sorry, try again!");
+		}
 	}
 }
-};
 
-function flipCard(cardId) {
+function flipCard() {
+	var cardId = this.getAttribute("data-id");
 	console.log("User flipped " + cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage);
 	cardsInPlay.push(cards[cardId].rank);
-	checkForMatch();
 	console.log(cards[cardId].suit);
 	console.log(cards[cardId].cardImage);
+	checkForMatch();
 }
 
+function createBoard() {
+	for(var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute("src", "images/back.png");
+		cardElement.setAttribute("data-id", i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
 
-flipCard(0);
-flipCard(2);
-
+createBoard();
 
 
 
